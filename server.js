b8c0017f1ca62519334527a53b77b0512b87b4f0
LoudySurya <63911353+LoudySurya@users.noreply.github.com>
2020-10-15 11:24:02 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+//inisialisasi library
+const express = require("express")
+const app = express()
+
+//import route siswa
+const siswa = require("./router/siswa")
+app.use("/siswa", siswa)
+
+
+//membuat web server dengan port 8000
+app.listen(8090, () => {
+    console.log("server run on port 8000")
+})
